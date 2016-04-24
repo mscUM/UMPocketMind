fe1c7506359af158bd31bb982da98605608cfd22
Eva <eva@gje.ch>
2016-04-24 16:52:58 +0200

    Created methods in TaskManager Class

--- app/src/main/java/de/umpocketmind/FunctionalityClasses/LocationManager.java
+++ app/src/main/java/de/umpocketmind/FunctionalityClasses/LocationManager.java
@@ -4,4 +4,5 @@ package de.umpocketmind.FunctionalityClasses;
  * Created by eva on 24.04.16.
  */
 public class LocationManager {
+
 }
--- app/src/main/java/de/umpocketmind/FunctionalityClasses/TaskManager.java
+++ app/src/main/java/de/umpocketmind/FunctionalityClasses/TaskManager.java
@@ -1,7 +1,38 @@
 package de.umpocketmind.FunctionalityClasses;
 
+import java.util.ArrayList;
+
 /**
  * Created by eva on 24.04.16.
  */
 public class TaskManager {
+
+    private static TaskManager taskManager;
+
+    public static TaskManager getTaskManager() {
+        if (taskManager == null) {
+            taskManager = new TaskManager();
+        }
+        return taskManager;
+    }
+
+    public void createTask() {
+        // Todo
+    }
+
+    public void updateTaskById(int id) {
+        // Todo
+    }
+
+    public void deleteTaskById(int id) {
+        // Todo
+    }
+
+    public void getTaskById(int id) {
+        // Todo
+    }
+
+    public void getAllTasks() {
+        // Todo
+    }
 }
