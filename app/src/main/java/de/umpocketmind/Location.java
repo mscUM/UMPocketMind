d4a58c59f0095111f9b72a3d7d8c3e80a51be0ba
Eva <eva@gje.ch>
2016-04-24 16:30:28 +0200

    Implemented Task Class

--- app/src/main/java/de/umpocketmind/Location.java
+++ app/src/main/java/de/umpocketmind/Location.java
@@ -4,4 +4,5 @@ package de.umpocketmind;
  * Created by eva on 24.04.16.
  */
 public class Location {
+
 }
--- app/src/main/java/de/umpocketmind/Task.java
+++ app/src/main/java/de/umpocketmind/Task.java
@@ -1,7 +1,56 @@
 package de.umpocketmind;
 
+import java.util.ArrayList;
+
 /**
  * Created by eva on 24.04.16.
  */
 public class Task {
+
+    private int id;                         // Unique identifier
+    private String name;                    // Task Name
+    private String description;             // Task Description
+    private double range;                   // Range to notify user
+    private ArrayList<Location> locations;  // Locations added to this task
+
+    // Constructor
+    public Task(int id, String name, String description, double range) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.range = range;
+        this.locations = new ArrayList<>();
+    }
+
+    // Getter and Setter of simple attributes
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getRange() {
+        return range;
+    }
+    public void setRange(double range) {
+        this.range = range;
+    }
+
+    // Method to add a location to a task
+    public void addLocationToTask(Location location) {
+        locations.add(location);
+    }
+
 }
