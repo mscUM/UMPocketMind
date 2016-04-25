8ba75af679ae1d580df0f83db7a9aea80fb682b6
Eva <eva@gje.ch>
2016-04-25 23:32:47 +0200

    Corrected warnings

--- app/src/main/java/de/umpocketmind/FunctionalityClasses/TaskLocationsManager.java
+++ app/src/main/java/de/umpocketmind/FunctionalityClasses/TaskLocationsManager.java
@@ -21,32 +21,30 @@ public class TaskLocationsManager {
     private Context context;
 
 
-    public TaskLocationsManager(Context context) {
+    protected TaskLocationsManager(Context context) {
         this.context = context;
         databaseConnectorTaskLocations = new DatabaseConnectorTaskLocations(context);
     }
 
-    public void open() {
+    protected void open() {
         taskLocationsDatabase = databaseConnectorTaskLocations.getWritableDatabase();
     }
 
-    public void close() {
+    protected void close() {
         databaseConnectorTaskLocations.close();
     }
 
-    public void createTaskLocation(Task task, Location location) {
+    protected void createTaskLocation(Task task, Location location) {
         ContentValues taskLocationValues = new ContentValues();
         taskLocationValues.put(DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_TASKID, task.getId());
         taskLocationValues.put(DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_LOCATIONID, location.getId());
-
-        long insertTaskLocationId = taskLocationsDatabase.insert(DatabaseConnectorTaskLocations.TABLE_TASKLOCATIONS, null, taskLocationValues);
     }
 
-    public ArrayList<Location> getAllTaskLocationsForTask(Task task) {
+    protected ArrayList<Location> getAllTaskLocationsForTask(Task task) {
         ArrayList<Location> locationList = new ArrayList<>();
 
         Cursor cursor = taskLocationsDatabase.query(DatabaseConnectorTaskLocations.TABLE_TASKLOCATIONS,
-                taskLocationsColumns, null, null, null, null, null);
+                taskLocationsColumns, DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_TASKID + "=" + task.getId(), null, null, null, null);
 
         cursor.moveToFirst();
 
@@ -63,10 +61,10 @@ public class TaskLocationsManager {
         return locationList;
     }
 
-    public void deleteAllTaskLocationsForTask(Task task) {
+    protected void deleteAllTaskLocationsForTask(Task task) {
 
         Cursor cursor = taskLocationsDatabase.query(DatabaseConnectorTaskLocations.TABLE_TASKLOCATIONS,
-                taskLocationsColumns, null, null, null, null, null);
+                taskLocationsColumns, DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_TASKID + "=" + task.getId(), null, null, null, null);
 
         cursor.moveToFirst();
 
@@ -80,24 +78,19 @@ public class TaskLocationsManager {
         cursor.close();
     }
 
-    public void deleteTaskLocationById(long id) {
+    protected void deleteTaskLocationById(long id) {
         taskLocationsDatabase.delete(DatabaseConnectorTaskLocations.TABLE_TASKLOCATIONS,
                 DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_ID + "=" + id,
                 null);
     }
 
     private Location cursorToTaskLocation(Cursor cursor) {
-        int idIndex = cursor.getColumnIndex(DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_ID);
-        int taskIdIndex = cursor.getColumnIndex(DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_TASKID);
         int locationIdIndex = cursor.getColumnIndex(DatabaseConnectorTaskLocations.COLUMN_TASKLOCATIONS_LOCATIONID);
 
-        long id = cursor.getLong(idIndex);
-        long taskId = cursor.getLong(taskIdIndex);
         long locationId = cursor.getLong(locationIdIndex);
 
         LocationManager locationManager = new LocationManager(context);
-        Location location = locationManager.getLocationById(locationId);
 
-        return location;
+        return locationManager.getLocationById(locationId);
     }
 }
