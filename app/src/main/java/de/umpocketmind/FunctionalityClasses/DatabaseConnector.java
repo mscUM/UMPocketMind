28f02aec85e868eb2789d87fc742bd3868425342
Eva <eva@gje.ch>
2016-04-25 19:36:37 +0200

    Created methods in DatabaseConnector Class

--- app/src/main/java/de/umpocketmind/FunctionalityClasses/DatabaseConnector.java
+++ app/src/main/java/de/umpocketmind/FunctionalityClasses/DatabaseConnector.java
@@ -4,4 +4,45 @@ package de.umpocketmind.FunctionalityClasses;
  * Created by eva on 24.04.16.
  */
 public class DatabaseConnector {
+
+    private static DatabaseConnector databaseConnector;
+
+    public static DatabaseConnector getDatabaseConnector() {
+        if (databaseConnector == null) {
+            databaseConnector = new DatabaseConnector();
+        }
+        return databaseConnector;
+    }
+
+    public void insertTaskInfo() {
+        // Todo
+    }
+
+    public void insertLocationInfo() {
+        // Todo
+    }
+
+    public void selectTaskInfo() {
+        // Todo
+    }
+
+    public void selectLocationInfo() {
+        // Todo
+    }
+
+    public void updateTaskInfo() {
+        // Todo
+    }
+
+    public void updateLocationInfo() {
+        // Todo
+    }
+
+    public void deleteTaskInfo() {
+        // Todo
+    }
+
+    public void deleteLocationInfo() {
+        // Todo
+    }
 }
