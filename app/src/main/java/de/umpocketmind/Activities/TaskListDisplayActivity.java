6d8372c6052e116ae87cfdbc1bd37a3a808fea32
janos12345 <janos.koch94@web.de>
2016-05-07 10:41:09 +0200

    fa

--- app/src/main/java/de/umpocketmind/Activities/TaskListDisplayActivity.java
+++ app/src/main/java/de/umpocketmind/Activities/TaskListDisplayActivity.java
@@ -5,54 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import java.util.ArrayList;
-import java.util.List;
-import de.umpocketmind.FunctionalityClasses.Task;
-import de.umpocketmind.FunctionalityClasses.TaskManager;
 import de.umpocketmind.R;
 public class TaskListDisplayActivity extends AppCompatActivity {
-    private TaskManager taskManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list_display);
-        taskManager = new TaskManager(this);
-    }
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        taskManager.open();
-        showAllTasks();
-        taskManager.close();
-    }
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        taskManager.close();
-    }
-    private void  showAllTasks()
-    {
-        //taskManager.insertMockDataIntoDatabase();
-        List<Task> taskList = taskManager.getAllTasks();
-        List<String> taskNames = new ArrayList<>();
-        for (Task task:taskList
-                ) {
-            taskNames.add(task.getName());
-        }
-        //locationManager.deleteAllTasksFromDatabase();
-        ArrayAdapter<String> taskArrayAdapter =
-                new ArrayAdapter<>
-                        (
-                                this,
-                                android.R.layout.select_dialog_item,
-                                taskNames
-                        );
-        ListView taskListView = (ListView) findViewById(R.id.Listview_TaskList);
-        taskListView.setAdapter(taskArrayAdapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,6 +22,7 @@ public class TaskListDisplayActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Specify what happens in case an action bar item is clicked
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
             return true;
         }
